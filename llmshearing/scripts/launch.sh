844cc21d6b3c9054ed80e1e299a0da3095f1e0db
zyzeng <zyzeng@example.com>
2024-04-29 20:00:38 +0800

    rename the path

--- llmshearing/scripts/launch.sh
+++ llmshearing/scripts/launch.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=test
-
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=4
-#SBATCH --mem=512gb
-#SBATCH --output=/remote-home/rypeng/0224/LLM-Shearing/logs/%x-%j.out
-
-
-PROJ_DIR=/remote-home/rypeng/0224/LLM-Shearing
-LOG_DIR=/remote-home/rypeng/0224/LLM-Shearing/logs
+PROJ_DIR="/remote-home/zyzeng/LLM-Shearing/LLM-Shearing"
 
 # num_nodes=$(scontrol show job $SLURM_JOB_ID | grep NodeList=della | wc -l)
 num_nodes=$(scontrol show hostnames $SLURM_JOB_NODELIST | wc -l)
@@ -27,5 +17,5 @@ echo "WORLD_SIZE="$WORLD_SIZE
 echo "num_nodes="$num_nodes
 
 if [[ $num_nodes == 1 ]]; then composer $PROJ_DIR/llmshearing/train.py $@; 
-else srun --output=$LOG_DIR/%x-%j-%n.out bash $PROJ_DIR/llmshearing/scripts/srun_launch.sh $@; fi
+else srun bash $PROJ_DIR/llmshearing/scripts/srun_launch.sh $@; fi
  
--- llmshearing/scripts/srun_launch.sh
+++ llmshearing/scripts/srun_launch.sh
@@ -1,4 +1,4 @@
-PROJ_DIR=/remote-home/rypeng/0224/LLM-Shearing
-
+PROJ_DIR=/remote-home/zyzeng/LLM-Shearing/LLM-Shearing
+cd ${PROJ_DIR}
 echo ${SLURM_NODEID} 
 composer --node_rank ${SLURM_NODEID} $PROJ_DIR/llmshearing/train.py $@  
