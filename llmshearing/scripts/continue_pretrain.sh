746098974d8ae9d487c635042c582710d0bfd78b
zyzeng <zyzeng@example.com>
2024-05-02 00:35:05 +0800

    add the script of pruning and continue training for 1.3b model

--- llmshearing/scripts/continue_pretrain.sh
+++ llmshearing/scripts/continue_pretrain.sh
@@ -1,27 +1,26 @@
-# pruning llama2 7b -> 3b or 1.3b
+# pruning internlm2 7b -> 3b or 1.3b
 
-PROJ_DIR=/scratch/gpfs/mengzhou/space2/LLM-Shearing
-DATA_DIR=/scratch/gpfs/mengzhou/llm_data/version5-uint16/500b_dedup_4k/for_ft
-OUTPUT_DIR=/scratch/gpfs/mengzhou/space2/out/test_release
+PROJ_DIR="/remote-home/zyzeng/LLM-Shearing/LLM-Shearing"
+DATA_DIR=${PROJ_DIR}/data_bin/moss_sampled/for_ft
+OUTPUT_DIR=${PROJ_DIR}/ckpts/
 LAUNCH_SCRIPT=${PROJ_DIR}/llmshearing/scripts/launch.sh
 TRAIN_SCRIPT=${PROJ_DIR}/llmshearing/train.py
 
 test=True
 
 model=1.3b # target model size
-config_file=${PROJ_DIR}/llmshearing/configs/llama2/${model}.yaml
-prune_run_name=llama2_7b_pruning_scaling_doremi_to${model}_sl4096
-path=${OUTPUT_DIR}/${prune_run_name}/pruned-latest-rank0.pt # path to the 
-# pruned model
-path=/scratch/gpfs/mengzhou/space2/out/test_round23_mosaicml_version5/llama2_7b_pruning_doremi_to1.3b_sl4096/changedkeys-ep0-ba3200-rank0.pt
+config_file=${PROJ_DIR}/llmshearing/configs/internlm/${model}.yaml
+prune_run_name=internlm2_7b_pruning_scaling_constant_to${model}_sl4096
+# path=${OUTPUT_DIR}/${prune_run_name}/pruned-latest-rank0.pt # path to the 
+path=${OUTPUT_DIR}/internlm_7b_pruning_scaling_constant_to1.3b_sl4096_v3/pruned-latest-rank0.pt
 
 # data setup
 data_local=${DATA_DIR}
 
 # basic setup
 max_seq_len=4096
-device_train_microbatch_size=16
-global_train_batch_size=256
+device_train_microbatch_size=8
+global_train_batch_size=384
 device_eval_batch_size=8
 
 # learning setup
@@ -32,44 +31,38 @@ t_warmup=1440ba # 3% learning rate warmup
 
 # dynamic loading setup
 dynamic=True
-set_names=[cc,github,book,stackexchange,wiki,arxiv,c4-rp] # domain names
-proportion=[0.2192,0.0002,0.0791,0.0064,0.0096,0.001,0.6845] # final proportion of pruning
+set_names=[arxiv,cn_baike,cn_book,cn_weixin,cn_zhihu,code_starcoder,en_book,en_stackexchange,wanjuan,wiki] # domain names
+proportion=[0.0038885832078429925,0.013171063389765087,6.913036813943098e-06,0.11492975550956504,0.06456880079775063,0.16964341743831857,0.0005333269641220821,0.07731814128971792,0.4785982974469239,0.07734170091917984] # initial proportion of RP, make sure that the sum(proportion) = 1
 # doremi: update weights with exponential descent
 # constant: keep the weights constant
-update_type=doremi 
-if [[ $to_model == 1.3b ]]; then
-    target_loss=[1.9643,0.7459,2.1393,1.6117,1.7590,1.4449,2.1251] # 1.3b predicted loss from scaling law
-else
-    target_loss=[1.8712,0.6883,2.0325,1.5353,1.6297,1.3560,2.0328] # 2.7b predicted loss from scaling law
-fi
+update_type=constant
+target_loss=[1.911728024482727,2.537081480026245,2.5637736320495605,2.378246545791626,2.9725301265716553,1.298712968826294,1.756330966949463,1.9188456535339355,2.548388957977295,1.7937837839126587] # 410m predicted loss from scaling law
+
 eval_split_name=eval_merge # eval on all domains
 eval_interval=400ba # eval every 50 batches and update the loading proportion
 
 
 # save directroy
-run_name=${prune_run_name}_ft${max_duration}
-save_dir=${OUTPUT_DIR}/${run_name}
-wandb_dir=${save_dir} # save locally
+run_name=${prune_run_name}_ft${max_duration}_v3
+save_dir=${OUTPUT_DIR}/finetune/${run_name}
 
-if [[ $test == True ]]; then t=00-01:00:00; else t=01-00:00:00; fi
 
 # Run with slurm
-# sbatch -p cli \
-#     --job-name ${run_name} \
-#     --nodes=8 \
-#     --gpus-per-node=2 \
-#     --mem=512gb \
-#     --cpus-per-task=8 \
-#     --time $t \
-#     $LAUNCH_SCRIPT \
-     
-
-# Run in bash, it will automatically use resources available in the current environment
-composer $TRAIN_SCRIPT \
+sbatch -p moss \
+    --job-name ${run_name} \
+    --nodes=6 \
+    --gpus-per-node=8 \
+    --mem=512gb \
+    --cpus-per-task=8 \
+    --output=/remote-home/zyzeng/LLM-Shearing/LLM-Shearing/logs/finetune/finetune_%A_%a.out \
+    --error=/remote-home/zyzeng/LLM-Shearing/LLM-Shearing/logs/finetune/finetune_%A_%a.err \
+    $LAUNCH_SCRIPT \
     $config_file \
     run_name=${run_name} \
     data_local=${data_local} \
     eval_loader.dataset.split=${eval_split_name} \
+    eval_loader.dataset.num_canonical_nodes=${global_train_batch_size} \
+    train_loader.dataset.num_canonical_nodes=${global_train_batch_size} \
     global_train_batch_size=${global_train_batch_size} \
     device_train_microbatch_size=${device_train_microbatch_size} \
     device_eval_batch_size=${device_eval_batch_size} \
@@ -78,7 +71,7 @@ composer $TRAIN_SCRIPT \
     eval_first=true \
     scheduler.t_warmup=${t_warmup} \
     save_folder=${save_dir} \
-    loggers.wandb.init_kwargs.dir=${wandb_dir} \
+    save_overwrite=False \
     eval_interval=${eval_interval} \
     save_interval=${save_interval} \
     optimizer.lr=${lr} \
@@ -92,6 +85,6 @@ composer $TRAIN_SCRIPT \
     train_loader.num_workers=0 \
     train_loader.prefetch_factor=null \
     train_loader.persistent_workers=false \
-    autoresume=false
+    autoresume=true
 
 # checking eval_first
--- llmshearing/scripts/pruning.sh
+++ llmshearing/scripts/pruning.sh
@@ -3,17 +3,17 @@
 # Please specify the working folder
 # PROJ_DIR=/scratch/gpfs/mengzhou/space2/LLM-Shearing
 # LAUNCH_SCRIPT=${PROJ_DIR}/llmshearing/scripts/launch.sh
-# DATA_DIR=/scratch/gpfs/mengzhou/llm_data/version5-uint16/500b_dedup_4k/for_prune
+# DATA_DIR=/scratch/gpfs/mengzhou/llm_data/version5-uint32/500b_dedup_4k/for_prune
 # OUTPUT_DIR=/scratch/gpfs/mengzhou/space2/out/test_release_pruning_full
 # TRAIN_SCRIPT=${PROJ_DIR}/llmshearing/train.py
 # MODEL_PATH=/projects/DANQIC/mengzhou/LLaMA2
 
-PROJ_DIR=/remote-home/rypeng/0224/LLM-Shearing
+PROJ_DIR=/remote-home/zyzeng/LLM-Shearing/LLM-Shearing
 LAUNCH_SCRIPT=${PROJ_DIR}/llmshearing/scripts/launch.sh
-DATA_DIR=/remote-home/rypeng/0224/LLM-Shearing/data/for_prune
-OUTPUT_DIR=/remote-home/rypeng/0224/LLM-Shearing/models/test-1b-composer
+DATA_DIR=${PROJ_DIR}/data_bin/moss_sampled/for_prune
+OUTPUT_DIR=${PROJ_DIR}/ckpts/
 TRAIN_SCRIPT=${PROJ_DIR}/llmshearing/train.py
-MODEL_PATH=/remote-home/rypeng/0224/LLM-Shearing/models/
+MODEL_PATH=${PROJ_DIR}/ckpts/
 
 # Specify $PROJ_DIR in scripts/launch.sh and scripts/srun_launch.sh if using slurm
 
@@ -21,8 +21,8 @@ test=False
 
 from_model=7b # source model size
 to_model=1.3b # target model size
-config_file=${PROJ_DIR}/llmshearing/configs/llama2/${from_model}.yaml
-path=$MODEL_PATH/mosaic-7B/state_dict.pt
+config_file=${PROJ_DIR}/llmshearing/configs/internlm/${from_model}.yaml
+path=$MODEL_PATH/internlm2_composer.pt
 
 # data setup
 data_local=${DATA_DIR}
@@ -36,22 +36,22 @@ device_eval_batch_size=8
 # learning setup
 lr=1e-4 # learning rate for the main parameters
 max_duration=3200ba # 0.42B tokens
-save_interval=3200ba # save in the end
+save_interval=1600ba # save in the end
 t_warmup=320ba # 10% learning rate warmup 
 
 # dynamic loading setup
 dynamic=True
-set_names=[cc,github,book,stackexchange,wiki,arxiv,c4-rp] # domain names
-proportion=[0.67,0.045,0.045,0.02,0.045,0.025,0.15] # initial proportion of RP, make sure that the sum(proportion) = 1
+set_names=[arxiv,cn_baike,cn_book,cn_weixin,cn_zhihu,code_starcoder,en_book,en_stackexchange,wanjuan,wiki] # domain names
+proportion=[0.0038885832078429925,0.013171063389765087,6.913036813943098e-06,0.11492975550956504,0.06456880079775063,0.16964341743831857,0.0005333269641220821,0.07731814128971792,0.4785982974469239,0.07734170091917984] # initial proportion of RP, make sure that the sum(proportion) = 1
 # doremi: update weights with exponential descent
 # constant: keep the weights constant
-update_type=doremi 
+update_type=constant 
 if [[ $to_model == 1.3b ]]; then
-    target_loss=[1.9643,0.7459,2.1393,1.6117,1.7590,1.4449,2.1251] # 1.3b predicted loss from scaling law
+    target_loss=[1.2465803623199463,3.0522592067718506,2.5978455543518066,2.5219357013702393,3.025589942932129,1.0684151649475098,2.2368016242980957,1.7007801532745361,2.288639783859253,1.634076714515686] # 1.3b predicted loss from scaling law
 elif [[ $to_model == 2.7b ]]; then
-    target_loss=[1.8712,0.6883,2.0325,1.5353,1.6297,1.3560,2.0328] # 2.7b predicted loss from scaling law
+    target_loss=[1.2465803623199463,3.0522592067718506,2.5978455543518066,2.5219357013702393,3.025589942932129,1.0684151649475098,2.2368016242980957,1.7007801532745361,2.288639783859253,1.634076714515686] # 2.7b predicted loss from scaling law
 elif [[ $to_model == 370m ]]; then
-    target_loss=[2.1401,0.8694,2.3625,1.7791,2.047,1.6637,2.3139] # 410m predicted loss from scaling law
+    target_loss=[1.2465803623199463,3.0522592067718506,2.5978455543518066,2.5219357013702393,3.025589942932129,1.0684151649475098,2.2368016242980957,1.7007801532745361,2.288639783859253,1.634076714515686] # 410m predicted loss from scaling law
 fi
 eval_split_name=eval_merge # eval on all domains
 eval_target_model=false # evaluate on the current model, not the target model, otherwise the loss will be inaccurate
@@ -62,19 +62,17 @@ eval_interval=50ba # eval every 50 batches and update the loading proportion
 lag_lr=1.0 # learning rate or l0_module
 lagr_warmup=640ba # 20% sparsity warmup
 if [[ $to_model == 1.3b ]]; then
-    target_d_model=2048; target_n_heads=16; target_n_layers=24; target_intermediate_size=5504
+    target_d_model=2048; target_n_heads=16; target_n_layers=24; target_intermediate_size=5504; target_vocab_size=92544
 elif [[ $to_model == 2.7b ]]; then
-    target_d_model=2560; target_n_heads=20; target_n_layers=32; target_intermediate_size=6912
+    target_d_model=2560; target_n_heads=20; target_n_layers=32; target_intermediate_size=6912; target_vocab_size=92544
 elif [[ $to_model == 370m ]]; then
-    target_d_model=1024; target_n_heads=8; target_n_layers=24; target_intermediate_size=2816
+    target_d_model=1024; target_n_heads=8; target_n_layers=24; target_intermediate_size=2816; target_vocab_size=92544
 fi
 
 # save directroy
-run_name=llama2_${from_model}_pruning_scaling_${update_type}_to${to_model}_sl${max_seq_len}
+run_name=internlm_${from_model}_pruning_scaling_${update_type}_to${to_model}_sl${max_seq_len}_v3
 save_dir=${OUTPUT_DIR}/${run_name}
 tensorboard_dir=${save_dir} # save locally
-
-if [[ $test == True ]]; then t=00-01:00:00; else t=00-20:00:00; fi
 
 # Run in bash, it will automatically use resources available in the current environment
 # composer $TRAIN_SCRIPT \
@@ -83,9 +81,10 @@ if [[ $test == True ]]; then t=00-01:00:00; else t=00-20:00:00; fi
 sbatch --job-name ${run_name} \
     --partition=moss \
     --nodes=1 \
-    --gpus-per-node=4 \
+    --gpus-per-node=8 \
     --mem=512gb \
-    --time $t \
+    --output=/remote-home/zyzeng/LLM-Shearing/LLM-Shearing/logs/prune/prune_%A_%a.out \
+    --error=/remote-home/zyzeng/LLM-Shearing/LLM-Shearing/logs/prune/prune_%A_%a.err \
     $LAUNCH_SCRIPT \
     $config_file \
     run_name=${run_name} \
@@ -96,7 +95,7 @@ sbatch --job-name ${run_name} \
     device_eval_batch_size=${device_eval_batch_size} \
     max_seq_len=${max_seq_len} \
     max_duration=${max_duration} \
-    eval_first=false \
+    eval_first=true \
     scheduler.t_warmup=${t_warmup} \
     save_folder=${save_dir} \
     eval_interval=${eval_interval} \
@@ -110,7 +109,9 @@ sbatch --job-name ${run_name} \
     model.l0_module.target_model.d_model=${target_d_model} \
     model.l0_module.target_model.n_heads=${target_n_heads} \
     model.l0_module.target_model.n_layers=${target_n_layers} \
+    model.l0_module.target_model.vocab_size=${target_vocab_size} \
     model.l0_module.target_model.intermediate_size=${target_intermediate_size} \
+    model.l0_module.start_sparsity=0 \
     callbacks.data_loading.dynamic=${dynamic} \
     callbacks.data_loading.set_names=${set_names} \
     callbacks.data_loading.proportion=${proportion} \
